e58a49851da63070144b065844762b6dadcfe760
Josh Cheek <josh.cheek@gmail.com>
2011-09-28 15:17:29 -0500

    Fix controller to not override or allow ordering

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -2,7 +2,10 @@ module Admin
   class MembersController < Admin::BaseController
 
     crudify :member,
-            :title_attribute => 'first_name', :xhr_paging => true
+            :title_attribute => 'first_name',
+            :xhr_paging => true,
+            :sortable => false,
+            :order => 'last_name ASC'
 
   end
 end
